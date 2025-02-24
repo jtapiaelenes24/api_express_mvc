f6c34e0cda3e6f8c4ff8e4f852498640fe812f14
JT <jtapiaelenes24@gmail.com>
2025-02-24 21:23:49 +0100

    Added CRUD operations for students table in the database in StudentsController.js + Added json Middleware and CORS in index.js

--- controllers/studentsController.js
+++ controllers/studentsController.js
@@ -3,24 +3,92 @@ class StudentsController {
   constructor() {}
 
   get(req, res) {
-    res.json({ msg: "Get students from class" });
+    try {
+      db.query(`SELECT * FROM students`, (err, rows) => {
+        if (err) {
+          res.status(400).send(err);
+        }
+        res.status(200).json(rows);
+      });
+    } catch (err) {
+      res.status(500).send(err.message);
+    }
   }
 
   getDetails(req, res) {
     const { id } = req.params;
-    res.json({ msg: `Get details of the student from class with ID ${id}` });
+    try {
+      db.query(`SELECT * FROM students WHERE id = ?`, [id], (err, rows) => {
+        if (err) {
+          res.status(400).sned(err);
+        }
+        res.status(200).json(rows[0]);
+      });
+    } catch (err) {
+      res.status(500).send(err.message);
+    }
   }
 
   insert(req, res) {
-    res.json({ msg: "Insert a student from class" });
+    try {
+      const { dni, name, lastname, email } = req.body;
+      db.query(
+        `INSERT INTO students
+        (id, dni, name, lastname, email)
+        VALUES(NULL, ?, ?, ?, ?);`,
+        [dni, name, lastname, email],
+        (err, rows) => {
+          if (err) {
+            res.status(400).send(err);
+          }
+          res.status(201).json({ id: rows.insertId });
+        }
+      );
+    } catch (err) {
+      res.status(500).send(err.message);
+    }
   }
 
   update(req, res) {
-    res.json({ msg: "Update a student from class" });
+    const { id } = req.params;
+    try {
+      const { dni, name, lastname, email } = req.body;
+      db.query(
+        `UPDATE students 
+        SET dni = ?, name = ?, lastname = ?, email = ? 
+        WHERE id = ?;`,
+        [dni, name, lastname, email, id],
+        (err, rows) => {
+          if (err) {
+            res.status(400).send(err);
+          }
+          if (rows.affectedRows == 1) {
+            res.status(200).json({ answer: "Register updated succesfully." });
+          }
+        }
+      );
+    } catch (err) {
+      res.status(500).send(err);
+    }
   }
 
   delete(req, res) {
-    res.json({ msg: "Delete a student from class" });
+    const { id } = req.params;
+    try {
+      db.query(
+        `DELETE FROM students
+        WHERE id = ?;`,
+        [id],
+        (err, rows) => {
+          if (err) {
+            res.status(400).send(err);
+          }
+          res.status(200).json({ answer: "User deleted succesfully." });
+        }
+      );
+    } catch (err) {
+      res.status(500).send(err);
+    }
   }
 }
 
--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const cors = require("cors");
 const studentsRoutes = require("./routes/studentsRoutes.js");
 const teachersRoutes = require("./routes/teachersRoutes.js");
 const app = express();
 
+//  MIDDLEWARE
+app.use(express.json());
+
+app.use(cors());
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -13,6 +18,3 @@ app.use("/teachers", teachersRoutes);
 app.listen(6500, () => {
   console.log("Servidor activo");
 });
-
-// https://www.youtube.com/watch?v=yd_QpXWrbtQ&list=PL4ONm-ifcbQJHg52qGN9GbMAhqK-mC2-y&index=4
-// 27:40
